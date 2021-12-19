71da2021c183655c2efe7750dda910302801aaf7
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2021-12-18 20:43:59 -0500

    Time: 149 ms (8.68%), Space: 38.6 MB (69.54%) - LeetHub

--- fibonacci-number/fibonacci-number.js
+++ fibonacci-number/fibonacci-number.js
@@ -2,18 +2,21 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
 
-    let sum = 0;
-    return calculateFib(n)
-    
+// memoization is used to solve dynamic programming problems
+// It stores duplicate subproblems
+// We will  use a js object
+// Keys will be the argument to the fxn and val will be the return val
+
+var fib = function(n, memo = {}) {
+    return calculateFib(n, memo)
 };
 
-var calculateFib = function(n){
-    if (n === 0 || n === 1) {
-        return n;
-    }
+var calculateFib = function(n, memo){
+    if (n in memo) return memo[n];
+    if (n === 0 || n === 1) return n;
     
-    sum = calculateFib(n - 1) + calculateFib(n - 2)
-    return sum;
+    memo[n] = calculateFib(n - 1, memo) + calculateFib(n - 2, memo );
+
+    return memo[n];
 }
