a4b10b2a2ac99b098921a5bbd2c20e4060f864cb
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-16 21:05:19 -0500

    Time: 142 ms (37.73%), Space: 49.3 MB (11.01%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -6,14 +6,14 @@ var containsDuplicate = function(nums) {
     let obj = {};
     
     for (num of nums) {
-        !obj[num] ? obj[num] = 1 : obj[num] += 1;
-    }
-    
-    const numCounts = Object.values(obj)
-    if (Math.max(...numCounts) > 1) {
-        return true;
+        if (obj[num]) {
+            return true
+        } else {
+            obj[num] = 1;
+        }
     }
     return false;
+    
 //     for (let i = 1; i < nums.length; i++) {
 //         const cur = nums[i];
 //         const prev = nums[i - 1];
