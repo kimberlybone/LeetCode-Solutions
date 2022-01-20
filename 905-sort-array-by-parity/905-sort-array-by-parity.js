9d5ee3b892aeafefe6716d8967f4c12c5147257f
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-19 21:09:37 -0500

    Time: 119 ms (42.69%), Space: 41 MB (67.78%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.js
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -6,18 +6,38 @@ var sortArrayByParity = function(nums) {
 //  loop and check for even nums then push them into an arr
 //     Then push all other (odd) nums into arr
     
-    let result = [];
+    let left = 0;
+    let right = nums.length - 1;
+    let temp;
     
-    for(let i = 0; i < nums.length; i++) {
-        if (nums[i] % 2 === 0) {
-            result.push(nums[i]);
+    while (left < right) { // 2 < 1
+
+        if ((nums[left] % 2 === 0) && (nums[right]%2 !== 0)){ // true, true
+            left++;
+            right--;
+        } else if ( (nums[left] % 2 !== 0) && (nums[right] % 2 !== 0) ) { 
+            right--;
+        } else if ( (nums[left] % 2 !== 0) && (nums[right] % 2 === 0) ) {
+            temp = nums[left];
+            nums[left] = nums[right]; 
+            nums[right] = temp;
+        } else if ( (nums[left] % 2 === 0) && (nums[right] % 2 === 0) ) { 
+            left++;
         }
+        
     }
+    return nums;
     
-    for(let j = 0; j < nums.length; j++) {
-        if (nums[j] % 2 !== 0) {
-            result.push(nums[j]);
-        }
-    }
-    return result;
+//     for(let i = 0; i < nums.length; i++) {
+//         if (nums[i] % 2 === 0) {
+//             result.push(nums[i]);
+//         }
+//     }
+    
+//     for(let j = 0; j < nums.length; j++) {
+//         if (nums[j] % 2 !== 0) {
+//             result.push(nums[j]);
+//         }
+//     }
+    // return result;
 };
