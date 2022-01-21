d5dea35408ab03c10aded0d90bf8b85540f1cf23
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-20 22:10:52 -0500

    Time: 282 ms (35.06%), Space: 49.1 MB (71.35%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+    let result = [];
+    let p2 = nums.length - 1;
+    
+    if (nums.length < 3) return result;
+    
+    nums.sort((a,b) => a - b);
+    
+    for(let i = 0; i < nums.length; i++) {
+        let p1 = i + 1;
+        let p2 = nums.length - 1;
+        
+        while (nums[i] === nums[i + 1]) i++;
+        while (p1 < p2) {
+            const sum = nums[p1] + nums[p2] + nums[i];
+            
+            if (sum === 0 && ((nums[p1] !== nums[p1 -1]) || (nums[p2] !== nums[p2 + 1]))) {
+                result.push([ nums[i], nums[p1], nums[p2] ]);
+                while (nums[p1] === nums[p1 + 1]) p1++;
+                while(nums[p2] === nums[p2 - 1]) p2--;
+                p1++;
+                p2--;
+            } else if (sum < 0)  p1++;
+            else  p2--;
+        }
+    }
+    return result;
+};
