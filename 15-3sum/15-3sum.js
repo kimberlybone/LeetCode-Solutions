d353ba6ef3054203fb033e8b0711ca11afd1edbb
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-20 22:13:03 -0500

    Time: 247 ms (44.81%), Space: 49 MB (71.35%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -15,18 +15,20 @@ var threeSum = function(nums) {
         let p1 = i + 1;
         let p2 = nums.length - 1;
         
-        while (nums[i] === nums[i + 1]) i++;
-        while (p1 < p2) {
-            const sum = nums[p1] + nums[p2] + nums[i];
+        if (nums[i] !== nums[i - 1]) {
             
-            if (sum === 0 && ((nums[p1] !== nums[p1 -1]) || (nums[p2] !== nums[p2 + 1]))) {
-                result.push([ nums[i], nums[p1], nums[p2] ]);
-                while (nums[p1] === nums[p1 + 1]) p1++;
-                while(nums[p2] === nums[p2 - 1]) p2--;
-                p1++;
-                p2--;
-            } else if (sum < 0)  p1++;
-            else  p2--;
+            while (p1 < p2) {
+                const sum = nums[p1] + nums[p2] + nums[i];
+
+                if (sum === 0 && ((nums[p1] !== nums[p1 -1]) || (nums[p2] !== nums[p2 + 1]))) {
+                    result.push([ nums[i], nums[p1], nums[p2] ]);
+                    while (nums[p1] === nums[p1 + 1]) p1++;
+                    while(nums[p2] === nums[p2 - 1]) p2--;
+                    p1++;
+                    p2--;
+                } else if (sum < 0)  p1++;
+                else  p2--;
+            }
         }
     }
     return result;
