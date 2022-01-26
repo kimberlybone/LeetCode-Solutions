574eea574156181dad650cda73397c2bff48821f
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-26 13:01:59 -0500

    Time: 71 ms (87.43%), Space: 39.6 MB (48.92%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -5,23 +5,21 @@
  */
 var minSubArrayLen = function(target, nums) {
     let start = 0;
-    let end = 1;
+    let end = 0;
     let sum = nums[start];
     let shortest = Infinity;
     
     if (sum === target) return end - start;
     
-    while ((end < nums.length + 1) && (start <= end)) {
-        let startChar = nums[start];
-        let endChar = nums[end];
+    while (end < nums.length) {
         
         if (sum >= target) {
-            shortest = Math.min(shortest, end - start);
+            shortest = Math.min(shortest, end - start + 1);
             sum -= nums[start];
             start++;
         } else if (sum < target) {
-            sum += nums[end];
             end++;
+            sum += nums[end];
         }
         
     }
