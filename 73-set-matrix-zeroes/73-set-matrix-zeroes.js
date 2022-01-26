b004152325d04405a3f7d6786f73fbee2fa4e427
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-25 22:40:35 -0500

    Time: 96 ms (78.56%), Space: 40.6 MB (98.65%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -9,21 +9,21 @@ var setZeroes = function(matrix) {
 //     2nd set will be columns
 //     loop to change all the numbers
     
-    let rows = new Set();
-    let columns = new Set()
+    let rows = [];
+    let columns = [];
     
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
             if (matrix[i][j] === 0) {
-                rows.add(i);
-                columns.add(j);
+                rows[i] = true;
+                columns[j] = true;
             }
         }
     }
     
         for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            if (rows.has(i) || columns.has(j)) {
+            if (rows[i] || columns[j]) {
                 matrix[i][j] = 0;
             }
         }
