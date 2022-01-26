08e2cde9f22e197adf607a5998a09df2530bb025
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-25 23:45:26 -0500

    Time: 109 ms (51.57%), Space: 41 MB (78.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -9,23 +9,67 @@ var setZeroes = function(matrix) {
 //     2nd set will be columns
 //     loop to change all the numbers
     
-    let rows = [];
-    let columns = [];
     
+    let zeroInFirstRow = false;
+    let zeroInFirstCol = false;
+    
+//     Go through first row
+    for (let i = 0; i < matrix[0].length; i++) {
+        zeroInFirstRow = zeroInFirstRow || matrix[0][i] === 0;
+    }
+//     Go through first column
     for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
+        zeroInFirstCol = zeroInFirstCol || matrix[i][0] === 0;
+    }
+    
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[i].length; j++) {
             if (matrix[i][j] === 0) {
-                rows[i] = true;
-                columns[j] = true;
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
             }
         }
     }
     
-        for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (rows[i] || columns[j]) {
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[i].length; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
                 matrix[i][j] = 0;
             }
         }
     }
+    
+//     Go through first row
+    for (let i = 0; i < matrix[0].length; i++) {
+        if(zeroInFirstRow) {
+            matrix[0][i] = 0;
+        }
+    }
+//     Go through first column
+    for (let i = 0; i < matrix.length; i++) {
+        if(zeroInFirstCol) {
+            matrix[i][0] = 0;
+        }
+    }
+    
+    
+//     let rows = [];
+//     let columns = [];
+    
+//     for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix[i].length; j++) {
+//             if (matrix[i][j] === 0) {
+//                 rows[i] = true;
+//                 columns[j] = true;
+//             }
+//         }
+//     }
+    
+//         for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix[i].length; j++) {
+//             if (rows[i] || columns[j]) {
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
 };
