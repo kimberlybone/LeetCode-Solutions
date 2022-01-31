44e9de957ff6089d63126adec4494949fd7bc18f
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-31 00:04:13 -0500

    Time: 158 ms (59.53%), Space: 52.2 MB (36.06%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.js
+++ 904-fruit-into-baskets/904-fruit-into-baskets.js
@@ -10,24 +10,24 @@ var totalFruit = function(fruits) {
 //     also 
     
     let start = 0;
-    let counts = {};
+    let counts = new Map();
     let max = 0;
     
     for (let end = 0; end < fruits.length; end++) {
         
-        counts[fruits[end]] = counts[fruits[end]] + 1 || 1
+        counts.set(fruits[end], counts.get(fruits[end]) + 1 || 1);
         
-        while (Object.keys(counts).length > 2) {
-            const fruitCount = counts[fruits[start]];
+        while (counts.size > 2) {
             const fruit = fruits[start];
+            const fruitCount = counts.get(fruit);
             
-            if (fruitCount - 1 === 0) delete counts[fruit];
-            else counts[fruit] = fruitCount - 1;
+            if (fruitCount - 1 === 0) counts.delete(fruit);
+            else counts.set(fruit, fruitCount - 1);
             
             start++;
         }
         
-        if (Object.keys(counts).length >= 1) max = Math.max(max, end - start + 1);
+        if (counts.size >= 1) max = Math.max(max, end - start + 1);
     }
     return max;
 };
