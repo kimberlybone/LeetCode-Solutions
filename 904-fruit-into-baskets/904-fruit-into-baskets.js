58d6fcb54b208dbc7f7dc18b10ddb8b2d5027482
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-01-30 23:57:09 -0500

    Time: 2465 ms (5.19%), Space: 66.6 MB (5.04%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function(fruits) {
+//     sort
+//     continue increasing right pointer 
+//     add to obj if it doesn't exist
+//     if count size exceeds 2, take out beginning of the obj since it is already in order
+//     also 
+    
+    let start = 0;
+    let counts = {};
+    let max = 0;
+    
+    for (let end = 0; end < fruits.length; end++) {
+        
+        !counts[fruits[end]] ? counts[fruits[end]] = 1 : counts[fruits[end]]++;
+        
+        while (Object.keys(counts).length > 2) {
+            const fruitCount = counts[fruits[start]];
+            const fruit = fruits[start];
+            
+            if (fruitCount - 1 === 0) delete counts[fruit];
+            else counts[fruit] = fruitCount - 1;
+            console.log(fruitCount)
+            
+            start++;
+        }
+        
+        if (Object.keys(counts).length >= 1) max = Math.max(max, end - start + 1);
+    }
+    return max;
+};
