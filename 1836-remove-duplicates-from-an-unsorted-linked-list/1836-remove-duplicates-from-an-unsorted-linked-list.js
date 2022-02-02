09402c19a8c3af43f845e095ed0dc7583edd829e
Kimberly Bone <49881771+kimberlybone@users.noreply.github.com>
2022-02-02 00:10:03 -0500

    Time: 1014 ms (6.06%), Space: 118.4 MB (6.06%) - LeetHub

---
+++ 1836-remove-duplicates-from-an-unsorted-linked-list/1836-remove-duplicates-from-an-unsorted-linked-list.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicatesUnsorted = function(head) {
+//     store values in map with key = node value & value = node value count
+//     
+    console.log(head)
+    
+    let current = head;
+    let countNodes = new Map();
+    
+    while (current !== null) {
+        countNodes.set(current.val, countNodes.get(current.val)+1 || 1);
+        
+        current = current.next;
+    }
+    
+    current = head;
+    let prev = null;
+    
+    while (current !== null) {
+        
+        if (countNodes.get(current.val) > 1 && current.val === head.val) {
+            head = head.next;
+        } else if (countNodes.get(current.val) > 1) {
+            prev.next = current.next;
+        } else {
+            prev = current;
+        }
+        
+        current = current.next;
+    }
+    return head;
+    
+};
